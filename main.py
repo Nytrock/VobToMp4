ca87a0478e4f2e73616197f5c24dc2be9f81328b
Nytrock <68463923+Nytrock@users.noreply.github.com>
2022-09-24 16:50:22 +0300

    Interface added

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import glob
+import stat
+
 import ffmpeg
 import os
 
 
 def main():
+    if not confirm_working('Welcome to VOB to mp4 converter. All ".VOB" files in the "video" folder will be '
+                           'converted to ".mp4" and placed in the "results" folder. '
+                           'Nothing will happen to the original files. And please do not interrupt the program! '
+                           '\nClear?', True):
+        write_to_console("The program has been interrupted.")
+        return
+
+    if not os.path.exists("videos"):
+        write_to_console('Error: Folder "videos" not found. Please create it in the same place where the '
+                         'application is located and place the files for conversion there.')
+        os.mkdir("videos")
+        return
+
     if not os.path.exists("result"):
         os.mkdir("result")
-    list_ = []
+    else:
+        if len([f for f in os.listdir("result")]) != 0:
+            if confirm_working('Files were found in the "results" folder. For the correct operation '
+                               'of the program, they will have to be removed.'):
+                clean_trash("result")
+            else:
+                write_to_console("The program has been interrupted.")
+                return
+
+    write_to_console("Starting the conversion... Don't be afraid to see a lot of text next..", True)
     for name in glob.glob("videos/*.VOB"):
         filename = name.split("\\")[1]
         result = "result/" + filename[0:filename.rfind(".")] + ".mp4"
         ffmpeg.input(name).output(result).run()
-        os.system('cls')
-        list_.append(filename + " - converted")
-        print("\n".join(list_))
+    write_to_console("All files have been successfully converted")
 
 
 def write_to_console(text: str, end=False) -> None:
@@ -22,6 +44,35 @@ def write_to_console(text: str, end=False) -> None:
         print(text)
     else:
         print(text, end='')
+
+
+def confirm_working(text: str, full=False) -> bool:
+    os.system('cls')
+    if full:
+        print(f"{text} (n/y)")
+    else:
+        print(f"{text} Continue? (n/y)")
+    while True:
+        answer = input()
+        if answer == "n":
+            return False
+        elif answer == "y":
+            return True
+        else:
+            print("Enter the correct answer (n - no, y - yes)")
+
+
+# Removing all files from the "materials" folder
+def clean_trash(path: str) -> None:
+    dirlist = [f for f in os.listdir(path)]
+    for f in dirlist:
+        fullname = os.path.join(path, f)
+        if os.path.isdir(fullname):
+            clean_trash(fullname)
+            os.rmdir(fullname)
+        else:
+            os.chmod(fullname, stat.S_IWRITE)
+            os.remove(fullname)
 
 
 if __name__ == '__main__':
