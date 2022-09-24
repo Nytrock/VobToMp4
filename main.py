a36710458d670c83a36eed018615c6b9187b833a
Nytrock <68463923+Nytrock@users.noreply.github.com>
2022-09-24 16:30:53 +0300

    Add main function

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ import glob
 import ffmpeg
 import os
 
-if not os.path.exists("result"):
-    os.mkdir("result")
-list_ = []
-for name in glob.glob("videos/*.VOB"):
-    filename = name.split("\\")[1]
-    result = "result/" + filename[0:filename.rfind(".")] + ".mp4"
-    ffmpeg.input(name).output(result).run()
+
+def main():
+    if not os.path.exists("result"):
+        os.mkdir("result")
+    list_ = []
+    for name in glob.glob("videos/*.VOB"):
+        filename = name.split("\\")[1]
+        result = "result/" + filename[0:filename.rfind(".")] + ".mp4"
+        ffmpeg.input(name).output(result).run()
+        os.system('cls')
+        list_.append(filename + " - converted")
+        print("\n".join(list_))
+
+
+def write_to_console(text: str, end=False) -> None:
     os.system('cls')
-    list_.append(filename + " - converted")
-    print("\n".join(list_))
+    if end:
+        print(text)
+    else:
+        print(text, end='')
+
+
+if __name__ == '__main__':
+    main()
