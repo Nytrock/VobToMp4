ec7c62fb70f0b4fdfc6a58d8c4511e1c40b3d017
Nytrock <68463923+Nytrock@users.noreply.github.com>
2022-12-25 00:22:42 +0300

    Prepare for create complete application

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def main():
     if not confirm_working('Welcome to VOB to mp4 converter. All ".VOB" files in the "video" folder will be '
                            'converted to ".mp4" and placed in the "results" folder. '
                            'Nothing will happen to the original files. And please do not interrupt the program! '
-                           '\nClear?', True):
+                           '\nClear?'):
         write_to_console("The program has been interrupted.")
+        input()
         return
 
     # Check for "videos" folder
@@ -19,6 +20,7 @@ def main():
         write_to_console('Error: Folder "videos" not found. Please create it in the same place where the '
                          'application is located and place the files for conversion there.')
         os.mkdir("videos")
+        input()
         return
 
     # Creating or cleaning "result" folder
@@ -27,38 +29,41 @@ def main():
     else:
         if len([f for f in os.listdir("result")]) != 0:
             if confirm_working('Files were found in the "results" folder. For the correct operation '
-                               'of the program, they will have to be removed.'):
+                               'of the program, they will have to be removed. Continue?'):
                 clean_trash("result")
             else:
                 write_to_console("The program has been interrupted.")
+                input()
                 return
 
     # Converting from VOB to mp4
-    write_to_console("Starting the conversion... Don't be afraid to see a lot of text next..", True)
+    write_to_console("Starting the conversion... Don't be afraid to see a lot of text next..")
     for name in glob.glob("videos/*.VOB"):
         name = os.path.abspath(name)
         filename = name.split("\\")[-1]
         result = "result/" + filename[0:filename.rfind(".")] + ".mp4"
         ffmpeg.input(name).output(result, ac=2).run()
     write_to_console("All files have been successfully converted")
+    input()
 
 
 # Write something to the console
-def write_to_console(text: str, end=False) -> None:
-    os.system('cls')
-    if end:
-        print(text)
+def write_to_console(text: str) -> None:
+    if os.name == 'nt':
+        x = os.system('cls')
     else:
-        print(text, end='')
+        x = os.system('clear')
+    print(text)
 
 
 # Confirmation of any action
-def confirm_working(text: str, full=False) -> bool:
-    os.system('cls')
-    if full:
-        print(f"{text} (n/y)")
+def confirm_working(text: str) -> bool:
+    if os.name == 'nt':
+        x = os.system('cls')
     else:
-        print(f"{text} Continue? (n/y)")
+        x = os.system('clear')
+
+    print(f"{text} (n/y)")
     while True:
         answer = input()
         if answer == "n":
